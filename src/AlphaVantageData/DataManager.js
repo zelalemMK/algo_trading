e64e9e1efc740303dd7f52deb063ddf8c2e00f72
Gavin Swofford <gavinswofford@gmail.com>
2021-03-08 22:14:34 -0600

    Fixed bugs with Alpha Vantage error handling

--- src/AlphaVantageData/DataManager.js
+++ src/AlphaVantageData/DataManager.js
@@ -3,7 +3,7 @@ import ApiKey from "../ApiKey/ApiKey";
 
 const AlphaDataManager = {
     getStock(ticker) {
-        return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker.ticker}&interval=5min&apikey=${ApiKey}`)
+        return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker.ticker}&interval=1min&apikey=${ApiKey}`)
         .then(result => result.json())
     },
 }
